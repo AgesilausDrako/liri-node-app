8a5bf776fd0080607bcf94399929208df966fcc1
AgesilausDrako <agesilaus@yahoo.com>
2017-07-31 11:47:01 -0400

    added Twitter keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'onGkbHAtBJMZDLpqPZwqLA7Sn',
+  consumer_secret: 'ZH5MN42NYTyIXk4I55kA4QVCgUDeFAAp3rfBELd2Ojz2k0tBvZ',
+  access_token_key: '890967173958709248-8SJiIS4o0IuH6KlPTibaYJrMpN1RtMb',
+  access_token_secret: 'DtAvVyTXfUcekPth4SKx8ijtnVBnwjJz7hW8QX0iluGr9',
+}
